3d1ce6fab3fbb5d93712765c7d7dc2400644ba19
simsononroad <d-github@kopaszhegy.hu>
2025-01-25 20:00:40 +0100

    I don't know what can I write here

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ try:
     GPIO.setwarnings(False) # Ignore warning for now
     #GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
     GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setwarnings(False)    # Ignore warning for now   # Use physical pin numbering
+    GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW)
+    GPIO.setup(20, GPIO.OUT, initial=GPIO.LOW)
+
     
     mode = "time"
     button_pressed = 0
@@ -20,7 +24,8 @@ try:
     
     def time():
         current_dateTime = datetime.now()
-
+        GPIO.output(21, GPIO.HIGH)
+        GPIO.output(20, GPIO.LOW)
         min = str(current_dateTime.minute)
         if len(min) == 1:
             display.print(f"{current_dateTime.hour}:0{min}")
@@ -30,6 +35,8 @@ try:
         
         
     def temp():
+        GPIO.output(20, GPIO.HIGH)
+        GPIO.output(21, GPIO.LOW)
         lekerdez = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q=pécel&appid=a3c3ac028697416ece9bd3c3a7c0f500&units=metric')
         jsonformatum = json.loads(lekerdez.text)
         temp = jsonformatum['main']['temp']
@@ -43,13 +50,23 @@ try:
         #print(round(jsonformatum['main']['temp']))
         #jsonformatum['main']['temp']
         
-            
+    def auto():
+        while True:
+            time()
+            sleep(10)
+            temp()
+            sleep(10)
+    
+    
     def main():
         global mode
         global button_pressed
         while True:
             if GPIO.input(18) == GPIO.HIGH:
-                change_mode()                
+                change_mode()
+            display_mode()
+            sleep(0.09)
+                      
 
     def change_mode():
         global mode
@@ -58,21 +75,30 @@ try:
         
         elif mode == "temp":
             mode = "time"
-        display_mode()
+        
+        elif mode == "temp":
+            mode = "auto"
+        
     def display_mode():
         global mode
         if mode == "time":
             time()
         elif mode == "temp":
             temp()
+        elif mode == "auto":
+            auto()
         
         
 
     if __name__ == "__main__":
         main()
 except KeyboardInterrupt:
+    GPIO.output(21, GPIO.HIGH)
+    GPIO.output(20, GPIO.HIGH)
     display.print('quit')
-    sleep(3)
+    sleep(1)
+    GPIO.output(21, GPIO.LOW)
+    GPIO.output(20, GPIO.LOW)
     display.fill(0)
     import os
     os.system('clear')
